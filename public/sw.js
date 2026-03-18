64a632e1162bf38674b40f6f5ed015345d6d6b2e
Digab2501 <gjdiabate@gmail.com>
2026-03-18 17:25:45 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,3 @@
-const CACHE = 'recuspro-v1';
-const ASSETS = ['/', '/index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
