0c5cca1d9e471714824ca2e5d3ff4ca3b0c85575
Digab2501 <gjdiabate@gmail.com>
2026-07-06 14:29:41 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  // Ne pas intercepter les requêtes vers Supabase
+  if (e.request.url.includes('supabase.co')) return;
+  e.respondWith(fetch(e.request));
+});
